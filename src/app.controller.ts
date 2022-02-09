34db767a760d929ba429681b2fc8e0efcd22928a
Wandy Santana <sntwandy@gmail.com>
2022-02-09 15:45:44 -0400

    Adding Auth strategy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,15 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // POST /login
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(): any {
-    return {};
+  login(@Request() req): any {
+    return req.user;
   }
 
   // GET /protected
